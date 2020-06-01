1b427a8583611c44ac3544823ac14e9fa2674d54
trofimov_d_a <derofim@y.ru>
2020-06-01 14:33:06 +0300

    REFACTOR: Find*.cmake

--- cmake/Findflex_support_headers.cmake
+++ cmake/Findflex_support_headers.cmake
@@ -8,8 +8,22 @@ find_package(chromium_build_util REQUIRED)
 
 list(REMOVE_AT CMAKE_MODULE_PATH -1)
 
-# uses Config.cmake or a -config.cmake file
-# see https://gitlab.kitware.com/cmake/community/wikis/doc/tutorials/How-to-create-a-ProjectConfig.cmake-file
-# BELOW MUST BE EQUAL TO find_package(... CONFIG REQUIRED)
-# NOTE: find_package(CONFIG) not supported with EMSCRIPTEN, so use include()
-include(${CMAKE_CURRENT_LIST_DIR}/cmake/flex_support_headers-config.cmake)
+if(NOT TARGET CONAN_PKG::flex_support_headers)
+  message(FATAL_ERROR "Use flex_support_headers from conan")
+endif()
+# conan package has '/include' dir
+set(flex_support_header_HEADER_FILE
+  ${CONAN_FLEX_SUPPORT_HEADERS_ROOT}/include/flex/cling_preloader.inc
+)
+if(flex_support_headers_LOCAL_BUILD)
+  # no '/include' dir on local build
+  set(flex_support_header_HEADER_FILE
+    ${CONAN_FLEX_SUPPORT_HEADERS_ROOT}/flex/cling_preloader.inc
+  )
+else()
+  # uses Config.cmake or a -config.cmake file
+  # see https://gitlab.kitware.com/cmake/community/wikis/doc/tutorials/How-to-create-a-ProjectConfig.cmake-file
+  # BELOW MUST BE EQUAL TO find_package(... CONFIG REQUIRED)
+  # NOTE: find_package(CONFIG) not supported with EMSCRIPTEN, so use include()
+  include(${CMAKE_CURRENT_LIST_DIR}/cmake/flex_support_headers-config.cmake)
+endif()
